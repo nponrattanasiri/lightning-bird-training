10809107d27856983f56d7d417e0c50e9b1af41f
n.ponrattanasiri <Icenp#29>
2020-04-09 19:14:48 +0700

    mission1

--- demo/demo/src/main/java/com/example/demo/SpringBootApplicationLuncher.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-import org.springframework.boot.SpringApplication;
-
-public class SpringBootApplicationLuncher {
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldController.class, args);
-    }
-}
